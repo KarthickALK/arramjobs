2fbed58a3026c5071daf4340b7a1213a623e428c
sowmiyadevi7 <sowmiyadevi378@gmail.com>
2023-08-01 14:55:29 +0530

    changes

--- application/controllers/RegistrationController.php
+++ application/controllers/RegistrationController.php
@@ -8,14 +8,14 @@ class RegistrationController extends CI_Controller {
 
     public function register() {
         $data = array(
-            'name' => $this->input->post('name'),
-            'mobile_number' => $this->input->post('phno'),
-            'email' => $this->input->post('email'),
-            'address' => $this->input->post('addr'),
-            'logo' =>$this->input->post('file'),
-            'name1'=>$this->input->post('name1'),
-            'mobile_number1' =>$this->input->post('phno1'),
-            'mail1' =>$this->input->post('mail1'),
+            'company_name' => $this->input->post('name'),
+            'company_mobile_number' => $this->input->post('phno'),
+            'company_email' => $this->input->post('email'),
+            'company_address' => $this->input->post('addr'),
+            'company_logo' =>$this->input->post('file'),
+            'name'=>$this->input->post('name1'),
+            'mobile_number' =>$this->input->post('phno1'),
+            'email' =>$this->input->post('mail1'),
             'role' =>$this->input->post('role')
         );
 
