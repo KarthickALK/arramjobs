f8bc0cb2f4f4b094c4ad8655d18b37466d1ce5ad
ksvlearning <ksvlearning@gmail.com>
2023-09-13 10:57:51 +0530

    loginchange

--- application/views/loginform.php
+++ application/views/loginform.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <title>Login Form</title>
+  <!-- karthickchanged -->
   <style>
     .btn-primary {
       background-color: #f39c12 !important;
